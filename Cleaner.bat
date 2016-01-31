1a610a16b32660767b6683dfbb6fb74c7b8d9ddb
Tempario17 <procer1224@yahoo.com.mx>
2016-01-31 13:48:28 -0500

    memory flash cleaner
    limpiador de archivos ocultos en USB

---
+++ Cleaner.bat
@@ -0,0 +1,92 @@
+@echo off
+title Memoria Flash Cleaner
+color a 
+@echo ----------------------------------------------
+@echo ---- REPARACION DE FICHEROS EN ---------------
+@echo ----------------------------------------------
+@echo Cambiando Atributo de Carpetas
+Attrib /d /s -r -h -s *.* 
+@echo ----------------------------------------------
+@echo Eliminado Accesos Directos
+if exist *.lnk del *.lnk
+if exist *Desktop.txt del *Desktop.txt /Q 
+@echo ----------------------------------------------
+@echo Eliminado Autorun
+if exist autorun.inf del autorun.inf /Q 
+if exist txluh.exe del txluh.exe 
+if exist txluhx.exe del txluhx.exe
+if exist hoaveo.exe del hoaveo.exe 
+if exist hoaveox.exe del hoaveox.exe
+if exist qeakie.exe del qeakie.exe 
+if exist qeakiex.exe del qeakiex.exe
+if exist foateo.exe del foateo.exe 
+if exist foateox.exe del foateox.exe 
+if exist seegoeo.exe del seegoeo.exe 
+if exist seegoeox.exe del seegoeox.exe 
+if exist feemuk.exe del feemuk.exe 
+if exist feemukxx.exe del feemukxx.exe 
+if exist gaiso.exe del gaiso.exe 
+if exist gaisox.exe del gaisox.exe 
+if exist voeowo.exe del voeowo.exe 
+if exist voeowox.exe del voeowox.exe 
+if exist sadbija.exe del sadbija.exe 
+if exist sadbijax.exe del sadbijax.exe 
+if exist napult.exe del napult.exe 
+if exist napultx.exe del napultx.exe 
+if exist jubila.exe del jubila.exe 
+if exist jubilax.exe del jubilax.exe 
+if exist sicilija.exe del sicilija.exe 
+if exist sicilijax.exe del sicilijax.exe 
+if exist keayoz.exe del keayoz.exe
+if exist keayozx.exe del keayozx.exe
+if exist xaiguaf.exe del xaiguaf.exe 
+if exist xaiguafx.exe del xaiguafx.exe
+if exist xaosud.exe del xaosud.exe
+if exist xaosudx.exe del xaosudx.exe
+if exist teiob.exe del teiob.exe
+if exist teiobx.exe del teiobx.exe
+if exist seeqoep.exe del seeqoep.exe
+if exist seeqoepx.exe del seeqoepx.exe
+if exist qeakie.exe del qeakie.exe
+if exist qeakiex.exe del qeakiex.exe
+if exist mzguic.exe del mzguic.exe
+if exist mzguicx.exe del mzguicx.exe
+if exist feumiu.exe del feumiu.exe
+if exist feumiux.exe del feumiux.exe
+if exist maupe.exe del maupe.exe
+if exist maupex.exe del maupex.exe
+if exist vaipem.exe del vaipem.exe
+if exist vaipemx.exe del vaipemx.exe
+if exist vsqud.exe del vsqud.exe
+if exist vsqudx.exe del vsqudx.exe
+if exist jypuev.exe del jypuev.exe
+if exist jypuevx.exe del jypuevx.exe
+if exist coiut.exe del coiut.exe
+if exist coiutx.exe del coiutx.exe
+if exist gaiso.exe del gaiso.exe
+if exist gaisox.exe del gaisox.exe
+if exist trkoy.exe del trkoy.exe
+if exist trkoyx.exe del trkoyx.exe
+if exist domoc.exe del domoc.exe
+if exist domocx.exe del domocx.exe
+if exist geali.exe del geali.exe
+if exist gealix.exe del gealix.exe
+if exist geali.exe del foateo.exe
+if exist gealix.exe del foateox.exe
+if exist geali.exe del keayoz.exe
+if exist gealix.exe del keayozx.exe
+if exist geali.exe del xaosud.exe
+if exist gealix.exe del xaosudx.exe
+if exist xeakoej.exe del xeakoej.exe
+if exist xeakoejx.exe del xeakoejx.exe
+if exist yealix.exe del yealix.exe
+if exist jauala.exe del jauala.exe
+@echo ----------------------------------------------
+@echo Operacion OK...
+@echo ----------------------------------------------
+@echo --------------------------------
+@echo - Elaborado por TEMPLARIO17 ...  -
+@echo --------------------------------  
+@echo ----------------------------------------------
+@echo Aporta al desarrollador 
+pause
